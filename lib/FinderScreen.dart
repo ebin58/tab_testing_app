a164700ca0c56eb48eda289c6bb14e9e33d4935b
ebin58 <sebastianebin83@gmail.com>
2025-02-26 20:48:46 -0500

    Fixed some spacing

--- lib/FinderScreen.dart
+++ lib/FinderScreen.dart
@@ -55,32 +55,32 @@ class PortState extends StatelessWidget{
 }
 
 class LandState extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
   
-            Icon(
-              Icons.map_rounded,
-              size: 150,
-            ),
+          Icon(
+            Icons.map_rounded,
+            size: 150,
+          ),
   
-            Expanded(
-              child: Column(
+          Expanded(
+            child: Column(
                 
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "Terpiez Finder",
-                    style: TextStyle(fontSize: 46, fontWeight: FontWeight.bold),
-                  ),
-                  Text("Closest Terpiez:"),
-                  Text("124.0m", style: TextStyle(fontSize: 24)),
-                ],
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+              Text(
+                "Terpiez Finder",
+                style: TextStyle(fontSize: 46, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
-        );
-    }
+              Text("Closest Terpiez:"),
+              Text("124.0m", style: TextStyle(fontSize: 24)),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
