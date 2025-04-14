c411ff50b61ccab6d5ba2fdfaaae399a70e59b4a
ebin58 <sebastianebin83@gmail.com>
2025-04-14 18:01:02 -0400

    Edited userData so that one id is persistant and will be used per user

--- lib/userData.dart
+++ lib/userData.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-
-class Userdata extends ChangeNotifier{
-  
-  String playerID = Uuid().v4();
+class Userdata extends ChangeNotifier {
+  String playerID = '';
   int numCaught = 0;
-  int dayPlayed =1; // default to initial use
-  DateTime firstLogin = DateTime(2025, 02, 1);
+  int dayPlayed = 1;
+  DateTime firstLogin = DateTime.now();
 
   Userdata() {
-    updateDaysPlayed(); 
-  }
-  
-
-  void incrementNumCaught() {
-    numCaught++;
-    notifyListeners(); // Notify UI to update
+    initUserdata();
   }
 
+  Future<void> initUserdata() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    // Load or create UUID
+    playerID = prefs.getString('playerID') ?? Uuid().v4();
+    if (!prefs.containsKey('playerID')) {
+      await prefs.setString('playerID', playerID);
+    }
+
+    // Load or set firstLogin date
+    if (prefs.containsKey('firstLogin')) {
+      firstLogin = DateTime.parse(prefs.getString('firstLogin')!);
+    } else {
+      firstLogin = DateTime.now();
+      await prefs.setString('firstLogin', firstLogin.toIso8601String());
+    }
+
+    updateDaysPlayed();
+  }
 
   void updateDaysPlayed() {
-    final currDate = DateTime.now();
-    dayPlayed = currDate.difference(firstLogin).inDays + 1;
+    final now = DateTime.now();
+    dayPlayed = now.difference(firstLogin).inDays + 1;
     notifyListeners();
   }
 
+  void incrementNumCaught() {
+    numCaught++;
+    notifyListeners();
+  }
 }
-
