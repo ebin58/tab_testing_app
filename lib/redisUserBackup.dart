5d4e7130fe5a8b551263eb3ed7bd265485766f1f
ebin58 <sebastianebin83@gmail.com>
2025-04-23 12:11:19 -0400

    Ok now I actually fixed fr fr

--- lib/redisUserBackup.dart
+++ lib/redisUserBackup.dart
@@ -9,12 +9,13 @@ Future<void> backupUserDataToRedis(String username, Userdata userData) async {
   final data = {
     userData.playerID: {
       'caught': userData.caughtList
-          .map((t) => {
+          .expand((t) => t.locations.map((loc) => {
                 'id': t.id,
-                'lat': t.locations.first.latitude,
-                'lon': t.locations.first.longitude,
+                'name': t.name,
+                'lat': loc.latitude,
+                'lon': loc.longitude,
                 'timestamp': DateTime.now().toIso8601String(),
-              })
+              }))
           .toList(),
       'stats': {
         'numCaught': userData.numCaught,
